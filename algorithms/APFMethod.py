dbe3118b356edee4137af2f82a7de813a6b359b2
Воронин Владимир <vladimirvoron7@gmail.com>
2022-03-07 15:03:36 +0300

    Start to implement APF method (vector surface)

---
+++ algorithms/APFMethod.py
@@ -0,0 +1,117 @@
+import math
+import random
+import time
+from abc import ABC
+
+from qgis.core import *
+from ModuleInstruments.Converter import Converter
+from ModuleInstruments.DebugLog import DebugLog
+from ModuleInstruments.FindPathData import FindPathData
+from algorithms.BaseAlgorithims.AlgorithmsBasedOnHallAndGrid import AlgoritmsBasedOnHallAndGrid
+from algorithms.BaseAlgorithims.SearchAlgorthim import SearchAlgorithm
+from algorithms.addition.GdalExtentions import ObjectsConverter
+from algorithms.addition.GeometryPointExpand import GeometryPointExpand
+from algorithms.addition.Visualizer import Visualizer
+
+
+class NodeAPF:
+    def __init__(self, point):
+        self.point = point
+
+
+class APFMethod(AlgoritmsBasedOnHallAndGrid, SearchAlgorithm, ABC):
+    def __init__(self, findpathdata: FindPathData, debuglog: DebugLog):
+        hall_width = 150
+        super().__init__(findpathdata, debuglog, hall_width)
+
+        cell_target = self.grid.difine_point(self.target_point_geometry)
+        self.target_point_expand = GeometryPointExpand(self.target_point, cell_target.n_row,
+                                                       cell_target.n_column)
+
+        self.point_search_distance = 2
+        self.point_search_distance_diagonal = self.point_search_distance * math.sqrt(2)
+        self.open_list = []
+        self.closed_list = []
+        self.all_nodes_list = []
+        self.last_node = None
+
+        self.list_of_path = []
+        self.final_path = []
+        self.is_succes = False
+
+    def __create_grid(self):
+        self.debuglog.start_block("create grid")
+        super()._create_grid()
+        self.debuglog.end_block("create grid")
+
+    def __get_shorter_path(self, feats, increase_points=0):
+        self.debuglog.start_block("get shorter path")
+        result = super()._get_shorter_path(feats, increase_points)
+        self.debuglog.end_block("get shorter path")
+        return result
+
+    def __set_geometry_to_grid(self):
+        self.debuglog.start_block("set geometry to grid")
+        super()._set_geometry_to_grid()
+        self.debuglog.end_block("set geometry to grid")
+
+    def __check_distance_to_target_point(self, node):
+        x_full_difference = self.target_point.x() - node.point_expand.point.x()
+        y_full_difference = self.target_point.y() - node.point_expand.point.y()
+        return math.sqrt(x_full_difference ** 2 + y_full_difference ** 2)
+
+    def __create_points_surface(self):
+        start_point = NodeAPF(self.starting_point)
+        self.open_list.append(start_point)
+        while len(self.open_list) != 0:
+
+
+
+    def run(self):
+        self.__create_points_surface()
+        self.final_path = self.__get_shorter_path(self.list_of_path)
+        self.visualise()
+
+    def visualise(self):
+        if self.create_debug_layers:
+            points_feats = ObjectsConverter.list_of_geometry_to_feats(self.all_nodes_list)
+            min_short_feats = ObjectsConverter.list_of_geometry_to_feats(self.list_of_path)
+            Visualizer.create_and_add_new_default_points(self.project, self.path_to_save_layers,
+                                                         points_feats)
+            Visualizer.create_and_add_new_path_short_tree(self.project, self.path_to_save_layers,
+                                                          min_short_feats)
+        Visualizer.create_and_add_new_final_path(self.project, self.path_to_save_layers, self.final_path)
+        if __name__ == '__main__':
+            points_geom = [QgsGeometry.fromPointXY(x.point_expand.point) for x in self.all_nodes_list]
+            Visualizer.update_layer_by_geometry_objects(
+                r"C:\Users\Neptune\Desktop\Voronin qgis\shp\points_import.shp", points_geom)
+            Visualizer.update_layer_by_geometry_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\min_path.shp",
+                                                        self.list_of_path)
+            Visualizer.update_layer_by_feats_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\short_path.shp",
+                                                     self.final_path)
+
+
+if __name__ == '__main__':
+    QgsApplication.setPrefixPath(r'C:\OSGEO4~1\apps\qgis', True)
+    qgs = QgsApplication([], False)
+    qgs.initQgis()
+    my_time = time.perf_counter()
+    n = 1
+    for i in range(n):
+        proj = QgsProject.instance()
+        proj.read(r'C:\Users\Neptune\Desktop\Voronin qgis\Voronin qgis.qgs')
+        point1 = QgsGeometry.fromPointXY(QgsPointXY(39.7867695,47.2744990))
+        point2 = QgsGeometry.fromPointXY(QgsPointXY(39.7794512, 47.2741065))
+        path = r"C:\Users\Neptune\Desktop\Voronin qgis\shp\Строения.shp"
+
+        obstacles = QgsVectorLayer(path)
+        source_list_of_geometry_obstacles = Converter.get_list_of_poligons_in_3395(obstacles, proj)
+        find_path_data = FindPathData(proj, point1, point2, obstacles, r"C:\Users\Neptune\Desktop\Voronin qgis\shp",
+                                      False,
+                                      source_list_of_geometry_obstacles)
+        debug_log = DebugLog()
+        check = AStarMethod(find_path_data, debug_log)
+        check.run()
+        print(debug_log.get_info())
+    my_time = (time.perf_counter() - my_time) / n
+    print(my_time)
