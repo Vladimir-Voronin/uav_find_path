9a250432e061fb243cc7524349dcfd4dea9186d6
Воронин Владимир <vladimirvoron7@gmail.com>
2022-03-27 19:48:51 +0300

    just check folder update

--- checks/check1.py
+++ checks/check1.py
@@ -1,2 +1,20 @@
+from memory_profiler import profile, memory_usage
+import tracemalloc
 
-print((3 + 2)//2)
+
+class Test:
+    def __init__(self):
+        self.my_list = []
+
+    def run(self):
+        # starting the monitoring
+        tracemalloc.start()
+        for i in range(10000):
+            self.my_list.append([i, i, i])
+            print(tracemalloc.get_traced_memory())
+            # stopping the library
+        tracemalloc.stop()
+
+
+a = Test()
+a.run()
