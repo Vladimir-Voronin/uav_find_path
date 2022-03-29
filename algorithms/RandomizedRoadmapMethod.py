f113bfc858a0f3f7d3173543f92e9b347017ec5f
Воронин Владимир <vladimirvoron7@gmail.com>
2022-03-29 19:46:45 +0300

    small rework

--- algorithms/RandomizedRoadmapMethod.py
+++ algorithms/RandomizedRoadmapMethod.py
@@ -1,8 +1,15 @@
+from abc import ABC
+
 from qgis.core import *
 from qgis.analysis import QgsGraph, QgsNetworkDistanceStrategy
+
+from ModuleInstruments.Converter import Converter
 from ModuleInstruments.DebugLog import DebugLog
 from ModuleInstruments.FindPathData import FindPathData
+from algorithms.BaseAlgorithims.AlgorithmsBasedOnHallAndGrid import AlgoritmsBasedOnHallAndGrid
+from algorithms.BaseAlgorithims.AlgorithmsBasedOnHallOnly import AlgorithmsBasedOnHallOnly
 from algorithms.GdalFPExtension.calculations.Graphs import GdalGraphSearcher
+from algorithms.GdalFPExtension.gdalObjects.Converter import ObjectsConverter
 from algorithms.GdalFPExtension.methodsInterfaces.SearchMethod import SearchMethodAbstract
 from algorithms.addition.Decorators import measuretime
 from algorithms.GdalFPExtension.qgis.visualization.Visualizer import Visualizer
@@ -16,17 +23,20 @@ import sys
 sys.path.insert(0, r'C:\OSGeo4W64\apps\Python37\lib')
 
 
-class RandomizedRoadmapMethod(SearchAlgorithm, SearchMethodAbstract):
+class RandomizedRoadmapMethod(AlgorithmsBasedOnHallOnly, SearchAlgorithm, SearchMethodAbstract, ABC):
     def __init__(self, findpathdata: FindPathData, debuglog: DebugLog):
-        super().__init__(findpathdata, debuglog)
-
-        self.hall = Hall(self.starting_point.x(), self.starting_point.y(), self.target_point.x(), self.target_point.y())
-
-        self.multi_polygon_geometry = self.hall.create_multipolygon_geometry_by_hall(self.obstacles, self.project)
+        hall_width = 150
+        super().__init__(findpathdata, debuglog, hall_width)
 
         # constants
         self.const_square_meters = 400
         self.const_sight_of_points = 12
+
+        self.min_short_path_tree_feats = []
+        self.random_points_feats = []
+        self.default_graph_feats = []
+        self.pre_final_path = []
+        self.final_path = []
 
     @measuretime
     def __create_graph(self):
@@ -63,6 +73,7 @@ class RandomizedRoadmapMethod(SearchAlgorithm, SearchMethodAbstract):
         vlayer1.dataProvider().addFeatures(feats)
         # endregion
         print("ST1")
+        self.random_points_feats = feats
 
         # creating graph
         qgs_graph = QgsGraph()
@@ -125,46 +136,72 @@ class RandomizedRoadmapMethod(SearchAlgorithm, SearchMethodAbstract):
             qgs_graph.addEdge(point1, point2, [QgsNetworkDistanceStrategy().cost(line.length(), feat)])
 
         vlayer2.dataProvider().addFeatures(feats_line)
+        self.default_graph_feats = feats_line
         return qgs_graph
 
     def run(self):
-        self.debug_info()
-        tick = time.perf_counter()
         graph = self.__create_graph()
         searcher = GdalGraphSearcher(graph, self.starting_point, self.target_point, 0)
 
         if not searcher.check_to_pave_the_way():
-            print("the algorithm failed to pave the way")
-            return None
+            raise QgsException("the algorithm failed to pave the way")
 
-        print("Length of min path is: ", searcher.min_length_to_vertex())
+        # get the shortest tree graph
+        self.min_short_path_tree_feats = searcher.get_shortest_tree_features_list()
 
-        # visualize the shortest tree graph
-        feats = searcher.get_shortest_tree_features_list()
-        Visualizer.update_layer_by_feats_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\short_tree.shp", feats)
+        self.debuglog.info("get_short_path_tree_feats")
 
-        # search min path and visualize
-        feats = searcher.get_features_from_min_path()
-        Visualizer.update_layer_by_feats_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\min_path.shp", feats)
-        acc = time.perf_counter() - tick
-        print(acc)
+        # get min path
+        self.pre_final_path = searcher.get_features_from_min_path()
 
-    def debug_info(self):
-        print("starting_point: ", self.starting_point)
-        print("target_point: ", self.target_point)
+        self.debuglog.info("get_pre_final_path")
+
+        self.final_path = self._get_shorter_path(self.pre_final_path, 2)
+
+        self.debuglog.info("get_final_path")
+
+    def visualize(self):
+        if self.create_debug_layers:
+            Visualizer.create_and_add_new_default_points(self.project, self.path_to_save_layers,
+                                                         self.random_points_feats)
+            Visualizer.create_and_add_new_default_graph(self.project, self.path_to_save_layers,
+                                                        self.default_graph_feats)
+            Visualizer.create_and_add_new_path_short_tree(self.project, self.path_to_save_layers,
+                                                          self.min_short_path_tree_feats)
+        Visualizer.create_and_add_new_final_path(self.project, self.path_to_save_layers, self.final_path)
+
+        if __name__ == '__main__':
+            Visualizer.update_layer_by_feats_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\points_import.shp",
+                                                     self.random_points_feats)
+            Visualizer.update_layer_by_feats_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\min_path.shp",
+                                                     self.pre_final_path)
+            Visualizer.update_layer_by_feats_objects(r"C:\Users\Neptune\Desktop\Voronin qgis\shp\short_path.shp",
+                                                     self.final_path)
 
 
 if __name__ == '__main__':
     QgsApplication.setPrefixPath(r'C:\OSGEO4~1\apps\qgis', True)
     qgs = QgsApplication([], False)
     qgs.initQgis()
+    my_time = time.perf_counter()
+    n = 1
+    for i in range(n):
+        proj = QgsProject.instance()
+        proj.read(r'C:\Users\Neptune\Desktop\Voronin qgis\Voronin qgis.qgs')
+        point1 = QgsGeometry.fromPointXY(QgsPointXY(4428094.059841852, 5955751.246513667))
+        point2 = QgsGeometry.fromPointXY(QgsPointXY(4428670.438183919, 5957666.393507188))
+        path = r"C:\Users\Neptune\Desktop\Voronin qgis\shp\Строения.shp"
 
-    proj = QgsProject.instance()
-    proj.read(r'C:\Users\Neptune\Desktop\Voronin qgis\Voronin qgis.qgs')
-    point1 = QgsGeometry.fromPointXY(QgsPointXY(39.7899186, 47.2674382))
-    point2 = QgsGeometry.fromPointXY(QgsPointXY(39.771948, 47.283142))
-    path = r"C:\Users\Neptune\Desktop\Voronin qgis\shp\Строения.shp"
-    obstacles = QgsVectorLayer(path)
-    print(obstacles)
-    check = RandomizedRoadmapMethod(point1, point2, obstacles, proj)
-    check.run()
+        obstacles = QgsVectorLayer(path)
+        source_list_of_geometry_obstacles = Converter.get_list_of_poligons_in_3395(obstacles, proj)
+        find_path_data = FindPathData(proj, point1, point2, obstacles, r"C:\Users\Neptune\Desktop\Voronin qgis\shp",
+                                      False,
+                                      source_list_of_geometry_obstacles)
+        debug_log = DebugLog()
+        check = RandomizedRoadmapMethod(find_path_data, debug_log)
+        my_time_full = 0
+        check.run()
+        check.visualize()
+        print(debug_log.get_info())
+    my_time = (time.perf_counter() - my_time) / n
+    print(my_time)
